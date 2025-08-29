13b3ab92d15e4c7029b256d2e23ee8d06c9bce20
kenny <khaneesh50@gmail.com>
2025-08-29 16:37:33 +0530

    feat: added vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import App from './App.tsx';
 import './index.css';
 import { HelmetProvider } from 'react-helmet-async';
 import { AuthProvider } from './contexts/AuthContext.tsx';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
       <AuthProvider>
         <App />
+        <SpeedInsights />
       </AuthProvider>
     </HelmetProvider>
   </StrictMode>
