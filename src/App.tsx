372dd9fd7767a77fb3ad2f8a97cbb42d79893a47
kenny <khaneesh50@gmail.com>
2025-12-19 16:08:07 +0530

    fix: final fix, works

--- src/App.tsx
+++ src/App.tsx
@@ -9,20 +9,19 @@ const App: React.FC = () => {
   return (
     <ErrorBoundary>
       <Router>
-        <Snowfall
-  style={{
-    position: "fixed",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    zIndex: 0,
-    width: "100vw",
-    height: "100vh",
-  }}
-/>
-        <div className="relative z-10 bg-transparent">
-          <AppRoutes />
+                <Snowfall
+                  style={{
+                    position: "fixed",
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    zIndex: 20,
+                    width: "100vw",
+                    height: "100vh",
+                  }}
+                />
+                <div className="relative z-0 bg-transparent">          <AppRoutes />
           <Toaster />
         </div>
       </Router>
