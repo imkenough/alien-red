ea29dd2977892a8eedab43ba18cdd021cdf5a3e6
kenny <khaneesh50@gmail.com>
2025-12-19 16:05:25 +0530

    fix: fixed snowfall

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,18 @@ const App: React.FC = () => {
   return (
     <ErrorBoundary>
       <Router>
-        <Snowfall className="fixed inset-0 z-0" />
+        <Snowfall
+  style={{
+    position: "fixed",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    zIndex: 0,
+    width: "100vw",
+    height: "100vh",
+  }}
+/>
         <div className="relative z-10 bg-transparent">
           <AppRoutes />
           <Toaster />
